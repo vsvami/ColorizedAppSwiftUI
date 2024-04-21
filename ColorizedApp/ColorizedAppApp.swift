edadba71d69a908fc8e5ff5bae698ae0962ca1fd
vsvami <106614918+vsvami@users.noreply.github.com>
2024-04-21 23:55:19 +0400

    Initial Commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Vladimir Dmitriev on 21.04.24.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
